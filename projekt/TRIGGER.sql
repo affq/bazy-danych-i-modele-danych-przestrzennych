5b821a3dafdf85cb4a4321e288b9277b15f374d4
affq <adrianfabisiewicz@gmail.com>
2024-01-20 20:52:18 +0100

    poddaję się

--- projekt/TRIGGER.sql
+++ projekt/TRIGGER.sql
@@ -5,9 +5,8 @@ create function ot_buzt_a_insert()
         insert into ot_buzt_p(geometria, rodzaj, idIIP, czyObiektBDOO, x_kod, x_katDoklGeom, x_doklGeom, x_zrodloDanychG, x_zrodloDanychA, x_rodzajReprGeom, x_uwagi, x_uzytkownik, x_aktualnoscG, x_aktualnoscA, x_cyklZycia, x_dataUtworzenia, x_informDodatkowa)
         values(st_centroid(new.geometria), new.rodzaj, new.idIIP, new.czyObiektBDOO, new.x_kod, new.x_katDoklGeom, new.x_doklGeom, new.x_zrodloDanychG, new.x_zrodloDanychA, new.x_rodzajReprGeom, new.x_uwagi, new.x_uzytkownik, new.x_aktualnoscG, new.x_aktualnoscA, new.x_cyklZycia, new.x_dataUtworzenia, new.x_informDodatkowa);
         return new;
-    end
-    $$
-    language plpgsql;
+    end;
+    $$ language plpgsql;
 
 create trigger tr_ot_buzt_a_insert
     before insert on ot_buzt_a
